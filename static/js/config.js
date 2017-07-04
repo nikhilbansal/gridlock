6f6382ee74869a45b8864f4a32b29740f6d5674f
Nikhil Bansal <nikhilbansi@gmail.com>
2017-07-04 08:53:08 +0530

    Final Commit

---
+++ static/js/config.js
@@ -0,0 +1,129 @@
+{
+  "center.x": 77.631374,
+  "center.y": 12.932688,
+  "map_type_id": "roadmap",
+  "zoom.start": 15,
+  "zoom.min": 1,
+  "zoom.max": 23,
+  "zoom.to_stops": 17,
+  "zoom.detail_view": 17,
+  "zoom.roadmap.stops_min": 15,
+  "zoom.roadmap.stops_max": 20,
+  "zoom.roadmap.shapes_min": 7,
+  "zoom.roadmap.shapes_max": 20,
+  "zoom.satellite.stops_min": 15,
+  "zoom.satellite.stops_max": 20,
+  "zoom.satellite.shapes_min": 7,
+  "zoom.satellite.shapes_max": 18,
+  "zoom.vehicle_follow": 17,
+  "zoom.vehicle_mouseover_min": 7,
+  "ft_layer_ids.mask": "1tDHsjdz7uhhAmWlmmwjR1P2Huf2LKMMiICPVdw",
+  "ft_layer_ids.gtfs_shapes": null,
+  "ft_layer_ids.gtfs_stops": null,
+  "ft_layer_ids.topology_edges": "1kcAk9AtrhC9B6qn2Ye0eKD9vcZS0GFmTaPXm5uhl",
+  "ft_layer_ids.topology_stations": "1YppDCNud7566oK_VwHsuUhGJqnm_CLDStMS3IuM",
+  "api_paths.trips": "api/demo/trips_hack_b.json",
+  "api_paths.departures": null,
+  "geojson.gtfs_shapes": null,
+  "geojson.gtfs_stops": null,
+  "geojson.topology_edges": "api/geojson/edges_hack.geojson",
+  "geojson.topology_stations": "api/geojson/stations_hack.geojson",
+  "routes": {
+    "cnl": {
+      "icon": "static/images/route_icons/demo/cnl.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "d": {
+      "icon": "static/images/route_icons/demo/d.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "ec": {
+      "icon": "static/images/route_icons/demo/ec.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "en": {
+      "icon": "static/images/route_icons/demo/en.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "ext": {
+      "icon": "static/images/route_icons/demo/ext.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "ic": {
+      "icon": "static/images/route_icons/demo/ic.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "ice": {
+      "icon": "static/images/route_icons/demo/ice.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "icn": {
+      "icon": "static/images/route_icons/demo/icn.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "ir": {
+      "icon": "static/images/route_icons/demo/ir.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "ire": {
+      "icon": "static/images/route_icons/demo/ire.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "r": {
+      "icon": "static/images/route_icons/demo/r.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "re": {
+      "icon": "static/images/route_icons/demo/re.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "rj": {
+      "icon": "static/images/route_icons/demo/rj.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "s": {
+      "icon": "static/images/route_icons/demo/s.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "tgv": {
+      "icon": "static/images/route_icons/demo/tgv.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    },
+    "zug": {
+      "icon": "static/images/route_icons/demo/zug.png",
+      "route_short_name": "",
+      "route_color": "0178BC",
+      "route_text_color": "FFFFFF"
+    }
+  }
+}
